83a2c9ad28766bdfe706bdd6d48b7d5bede93903
Alexandr Polyakov <alexandr.p@cronix.ms>
2018-06-26 14:00:57 +0300

    add validators and form-array to export

--- src/yoba-form.ts
+++ src/yoba-form.ts
@@ -1,7 +1,4 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
 export * from './form-group'
-// export * from './form-array';
+export * from './form-array'
 export * from './fill-object'
+export * from './validators'
